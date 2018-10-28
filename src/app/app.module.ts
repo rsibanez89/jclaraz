31943267a21a2229d7d51de8f127158f955dbc4d
rodrigo.ibanez <rodrigo.ibanez@rokt.com>
2018-10-28 18:50:10 +1100

    Added images and styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FlexLayoutModule } from '@angular/flex-layout/'
-import { MaterialModule } from './external/material.module'
-import { ReusableModule } from './reusable/reusable.module'
-import { SiteModule } from './site/site.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout/';
+import { MaterialModule } from './external/material.module';
+import { ReusableModule } from './reusable/reusable.module';
+import { SiteModule } from './site/site.module';
 
 
 @NgModule({
--- src/app/site/site.module.ts
+++ src/app/site/site.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
   declarations: [
     HomeComponent
